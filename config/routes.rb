6594c8879b33a0c0670ed88181c855d41805187e
ngocpha301 <ngocphat301@gmail.com>
2018-09-26 11:00:09 +0700

    index hotel 1

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
-
+  resources :hotels, only: %i(index show)
   namespace :admin do
     resources :hotels
   end
